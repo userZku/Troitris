9b1e787eb2cc258984fbd71da08148b5448ea3df
Théo C <89523113+userZku@users.noreply.github.com>
2024-05-22 17:10:57 +0200

    Floor + Boundaries

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    [SerializeField] Transform cameraLookAtPoint;
+    float angle;
+    const float cameraDistance = 60;
+
+    void Update()
+    {
+        angle += 0.3f * Time.deltaTime;
+        transform.position = new Vector3(cameraLookAtPoint.position.x + Mathf.Cos(angle) * cameraDistance,
+                                         cameraLookAtPoint.position.y + 20f,
+                                         cameraLookAtPoint.position.z + Mathf.Sin(angle) * cameraDistance);
+    }
+}
